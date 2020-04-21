3a48aed3fa10d8ceacc76c4c6fe67df8ef9e8114
rbellens <rik.bellens@telin.ugent.be>
2020-04-21 15:37:12 +0200

    remove quantity type argument

--- lib/src/measure.dart
+++ lib/src/measure.dart
@@ -6,8 +6,7 @@ import 'package:collection/collection.dart';
 import 'package:quiver/core.dart';
 
 /// The measurable, countable, or comparable property or aspect of a thing.
-abstract class Measurable<Q extends Quantity>
-    implements Comparable<Measurable<Q>> {
+abstract class Measurable implements Comparable<Measurable> {
   const Measurable();
 
   /// Returns the value of this measurable stated in the specified unit as a
@@ -23,12 +22,12 @@ abstract class Measurable<Q extends Quantity>
   Unit get standardUnit;
 
   @override
-  int compareTo(Measurable<Q> other) => Comparable.compare(
+  int compareTo(Measurable other) => Comparable.compare(
       doubleValue(standardUnit), other.doubleValue(standardUnit));
 }
 
 /// The result of a measurement stated in a known unit.
-abstract class Measure<V, Q extends Quantity> extends Measurable<Q> {
+abstract class Measure<V> extends Measurable {
   const Measure();
 
   /// The measurement value of this measure.
@@ -39,7 +38,7 @@ abstract class Measure<V, Q extends Quantity> extends Measurable<Q> {
 
   /// Returns the measure equivalent to this measure but stated in the
   /// specified unit.
-  Measure<V, Q> to(Unit unit);
+  Measure<V> to(Unit unit);
 
   @override
   Unit get standardUnit => unit;
@@ -49,7 +48,7 @@ abstract class Measure<V, Q extends Quantity> extends Measurable<Q> {
 }
 
 /// Scalar measure.
-class NumericMeasure<Q extends Quantity> extends Measure<num, Q> {
+class NumericMeasure extends Measure<num> {
   @override
   final num value;
 
@@ -59,9 +58,9 @@ class NumericMeasure<Q extends Quantity> extends Measure<num, Q> {
   const NumericMeasure(this.value, this.unit) : super();
 
   @override
-  Measure<num, Q> to(Unit unit) {
+  Measure<num> to(Unit unit) {
     if (unit == this.unit) return this;
-    return NumericMeasure<Q>(doubleValue(unit), unit);
+    return NumericMeasure(doubleValue(unit), unit);
   }
 
   @override
@@ -79,24 +78,23 @@ class NumericMeasure<Q extends Quantity> extends Measure<num, Q> {
 }
 
 /// A measurement vector of two or more dimensions.
-abstract class VectorMeasure<Q extends Quantity>
-    extends Measure<List<double>, Q> {
+abstract class VectorMeasure extends Measure<List<double>> {
   VectorMeasure._() : super();
 
   /// Creates a 2-dimensional measurement vector.
   factory VectorMeasure.twoDimensional(double x, double y, Unit unit) =
-      _TwoDimensional<Q>;
+      _TwoDimensional;
 
   /// Creates a 3-dimensional measurement vector.
   factory VectorMeasure.threeDimensional(
-      double x, double y, double z, Unit unit) = _ThreeDimensional<Q>;
+      double x, double y, double z, Unit unit) = _ThreeDimensional;
 
   /// Creates a multi-dimensional measurement vector.
   factory VectorMeasure.multiDimensional(List<double> components, Unit unit) =
-      _MultiDimensional<Q>;
+      _MultiDimensional;
 }
 
-class _TwoDimensional<Q extends Quantity> extends VectorMeasure<Q> {
+class _TwoDimensional extends VectorMeasure {
   final double x;
 
   final double y;
@@ -117,14 +115,14 @@ class _TwoDimensional<Q extends Quantity> extends VectorMeasure<Q> {
   List<double> get value => [x, y];
 
   @override
-  _TwoDimensional<Q> to(Unit unit) {
+  _TwoDimensional to(Unit unit) {
     if (unit == this.unit) return this;
     var cvtr = this.unit.getConverterTo(unit);
-    return _TwoDimensional<Q>(cvtr.convert(x), cvtr.convert(y), unit);
+    return _TwoDimensional(cvtr.convert(x), cvtr.convert(y), unit);
   }
 }
 
-class _ThreeDimensional<Q extends Quantity> extends VectorMeasure<Q> {
+class _ThreeDimensional extends VectorMeasure {
   final double x;
 
   final double y;
@@ -147,16 +145,16 @@ class _ThreeDimensional<Q extends Quantity> extends VectorMeasure<Q> {
   List<double> get value => [x, y, z];
 
   @override
-  _ThreeDimensional<Q> to(Unit unit) {
+  _ThreeDimensional to(Unit unit) {
     if (unit == this.unit) return this;
     var cvtr = this.unit.getConverterTo(unit);
-    return _ThreeDimensional<Q>(
+    return _ThreeDimensional(
         cvtr.convert(x), cvtr.convert(y), cvtr.convert(z), unit);
   }
 }
 
 // Holds multi-dimensional implementation.
-class _MultiDimensional<Q extends Quantity> extends VectorMeasure<Q> {
+class _MultiDimensional extends VectorMeasure {
   @override
   final List<double> value;
 
@@ -179,9 +177,9 @@ class _MultiDimensional<Q extends Quantity> extends VectorMeasure<Q> {
   }
 
   @override
-  _MultiDimensional<Q> to(Unit unit) {
+  _MultiDimensional to(Unit unit) {
     if (unit == this.unit) return this;
     var cvtr = this.unit.getConverterTo(unit);
-    return _MultiDimensional<Q>(value.map(cvtr.convert), unit);
+    return _MultiDimensional(value.map(cvtr.convert), unit);
   }
 }
